21f83afbd60e086dd252240e7cb74fa11e91099e
Bryan Tellefsen <BTellefsen42@gmail.com>
2021-05-26 10:41:58 -0400

    adding API

--- randomdrink/src/containers/App.js
+++ randomdrink/src/containers/App.js
@@ -1,50 +1,81 @@
 import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
-    
+
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+  constructor() {
+    super()
+    this.state = {
+      drink: null
+    };
+  }
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users => this.setState({ robots: users}));
-        }
+  componentDidMount() {
+    fetch('https://thecocktaildb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(data => this.setState({ drink: data.strDrink }));
+  }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+  render() {
+    const { drink } = this.state;
+    return (
+      <div>
+        <div>Drink: {drink}</div>
+      </div>
+    );
+  }
+}
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return !robots.length ? 
-            <h1>Loading</h1> :
-            (
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundry>
-                            <CardList robots={filteredRobots}/>
-                        </ErrorBoundry>
-                    </Scroll>
-                </div>
-            );
-        }
-    }
+// class App extends Component {
+//   state = {
+//     loading: true,
+//     drink: null
+//   };
 
+//   componentDidMount() {
+//     const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+//     const response = fetch(url);
+//     const data = response.json();
+//     this.setState({ drink: data.results[1], loading: false });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.loading || !this.state.drink ? (
+//           <div>loading...</div>
+//         ) : (
+//           <div>
+//             <div>{this.state.drink.strDrink}</div>
+//             <img src={this.state.drink.strDrinkThumb}/>
+//           </div>
+//         )}
+//         </div>
+//       )
+//   }
+// }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src='https://images.unsplash.com/photo-1570716893556-8cae2e541ba8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80' className="App-logo" alt="logo" />
+//         <Card />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- randomdrink/src/index.js
+++ randomdrink/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+// import Card from './components/Card';
 import reportWebVitals from './reportWebVitals';
-import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-  <App />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
